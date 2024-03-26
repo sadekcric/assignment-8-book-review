fcc7918ae273d51dcc13d52fc7c63ecbe10fb75a
sadekcric4 <sadekcric4@gmail.com>
2024-03-26 19:13:27 +0600

    Contuct us pages uploaded

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,73 +1,78 @@
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact Us</h1>
+    <div className="min-h-[calc(100vh-144px)] space-y-3 mt-10">
+      <h1 className=" text-2xl lg:text-4xl playFire font-bold underline text-center mb-5">Contact Us</h1>
 
-      <p>
-        Thank you for visiting [Your Website Name]! We value your feedback, inquiries, and suggestions. Whether you have questions about our
-        services, need assistance with your account, or simply want to reach out, we're here to help.
+      <p className="text-lg lg:text-xl leading-normal">
+        Thank you for visiting Book Vibe! We value your feedback, inquiries, and suggestions. Whether you have questions about our services,
+        need assistance with your account, or simply want to reach out, we're here to help.
       </p>
 
-      <h3>Customer Support:</h3>
+      <h3 className="text-xl lg:text-2xl font-bold underline">Customer Support:</h3>
 
-      <p>
+      <p className="text-lg lg:text-xl leading-normal">
         Our dedicated customer support team is available to assist you with any issues or concerns you may have. Please feel free to reach
         out to us via email or phone:
       </p>
 
-      <ul>
-        <li>Email: support@example.com</li>
-        <li>Phone: +1 (800) 123-4567</li>
+      <ul className="list-disc ml-7">
+        <li className="text-lg lg:text-xl font-semibold">Email: sadekcric4@gmail.com</li>
+        <li className="text-lg lg:text-xl font-semibold">Phone: +880 1933 287870</li>
       </ul>
 
-      <p>We strive to respond to all inquiries promptly, typically within 24-48 hours during regular business hours.</p>
+      <p className="text-lg lg:text-xl leading-normal">
+        We strive to respond to all inquiries promptly, typically within 24-48 hours during regular business hours.
+      </p>
 
-      <h3>Technical Support:</h3>
+      <h3 className="text-xl lg:text-2xl font-bold underline">Technical Support:</h3>
 
-      <p>
+      <p className="text-lg lg:text-xl leading-normal">
         Encountering technical difficulties? Our technical support team is ready to troubleshoot and resolve any issues you may encounter
         while using our website. Get in touch with us at:
       </p>
 
-      <ul>
-        <li>Email: techsupport@example.com</li>
-        <li>Phone: +1 (800) 987-6543</li>
+      <ul className="list-disc ml-7">
+        <li className="text-lg lg:text-xl font-semibold">Email: sadekcric4@gmail.com</li>
+        <li className="text-lg lg:text-xl font-semibold">Phone: +880 1933 287870</li>
       </ul>
 
-      <h3>Feedback and Suggestions:</h3>
+      <h3 className="text-xl lg:text-2xl font-bold underline">Feedback and Suggestions:</h3>
 
-      <p>
-        We're always looking for ways to improve and enhance your experience on [Your Website Name]. If you have any feedback, suggestions,
-        or feature requests, we'd love to hear from you! Send us an email at feedback@example.com and let us know how we can serve you
-        better.
+      <p className="text-lg lg:text-xl leading-normal">
+        We're always looking for ways to improve and enhance your experience on Book Vibe. If you have any feedback, suggestions, or feature
+        requests, we'd love to hear from you! Send us an email at feedback@example.com and let us know how we can serve you better.
       </p>
 
-      <h3>Media Inquiries:</h3>
+      <h3 className="text-xl lg:text-2xl font-bold underline">Media Inquiries:</h3>
 
-      <p>For media inquiries, partnership opportunities, or press-related matters, please contact our PR team at pr@example.com.</p>
+      <p className="text-lg lg:text-xl leading-normal">
+        For media inquiries, partnership opportunities, or press-related matters, please contact our PR team at pr@example.com.
+      </p>
 
-      <h3>Business Inquiries:</h3>
+      <h3 className="text-xl lg:text-2xl font-bold underline">Business Inquiries:</h3>
 
-      <p>
-        Interested in partnering with [Your Website Name] or exploring business opportunities? Reach out to our business development team at
+      <p className="text-lg lg:text-xl leading-normal">
+        Interested in partnering with Book Vibe or exploring business opportunities? Reach out to our business development team at
         bizdev@example.com.
       </p>
 
-      <h3>Social Media:</h3>
+      <h3 className="text-xl lg:text-2xl font-bold underline">Social Media:</h3>
 
-      <p>Stay connected with us on social media for updates, announcements, and exclusive content:</p>
+      <p className="text-lg lg:text-xl leading-normal">
+        Stay connected with us on social media for updates, announcements, and exclusive content:
+      </p>
 
-      <ul>
-        <li>Facebook: YourWebsiteName</li>
-        <li>Twitter: @YourWebsiteName</li>
-        <li>Instagram: @YourWebsiteName</li>
+      <ul className="list-disc ml-7">
+        <li className="text-lg lg:text-xl font-semibold">Facebook: MD Sadekur Rahman</li>
+        <li className="text-lg lg:text-xl font-semibold">Twitter: @sadekcric</li>
+        <li className="text-lg lg:text-xl font-semibold">Instagram: @sadekcric4</li>
       </ul>
 
-      <address>
-        <h2>[Your Website Name]</h2>
+      <address className="mt-5 text-right">
+        <h2 className="text-lg lg:text-xl font-semibold">Book Vibe</h2>
 
-        <p>123 Main Street</p>
-        <p>City, State, ZIP Code Country</p>
+        <p className="text-lg lg:text-xl leading-normal">1710 model para</p>
+        <p className="text-lg lg:text-xl leading-normal">Chowgacha, Jessore, Bangladesh.</p>
       </address>
     </div>
   );
